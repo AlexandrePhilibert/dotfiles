c698c67386729636c972295631d7f9fedf3e1bec
Alexandre Philibert <alexandre.philibert@ik.me>
2022-08-12 11:32:09 +0200

    Setup typescript lsp

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,13 @@
+require("lspconfig").tsserver.setup {
+    on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+        vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
+        vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
+        vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
+        vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, { buffer = 0})
+    end
+}
