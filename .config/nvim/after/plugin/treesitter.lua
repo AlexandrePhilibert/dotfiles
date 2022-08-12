ec00ed67473b6748c5a7254869eb2104f009686f
Alexandre Philibert <alexandre.philibert@ik.me>
2022-08-12 15:23:29 +0200

    Configure treesitter

---
+++ .config/nvim/after/plugin/treesitter.lua
@@ -0,0 +1,15 @@
+require('nvim-treesitter.configs').setup {
+    ensure_installed = { 
+        "javascript", 
+        "typescript", 
+        "tsx", 
+        "rust", 
+        "html", 
+        "json",
+        "toml",
+        "markdown",
+        "go",
+        "python",
+        "lua"
+    },
+}
