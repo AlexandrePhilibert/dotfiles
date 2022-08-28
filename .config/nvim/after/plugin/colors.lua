c200de1129253827dabfade237b43a8b04e09564
Alexandre Philibert <alexandre.philibert@ik.me>
2022-08-28 22:54:59 +0200

    Change colors of nvim-cmp menu

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -7,12 +7,10 @@ vim.cmd("hi clear SignColumn")
 hi("ColorColumn", { ctermbg=234 })
 hi("LineNr", { ctermbg=234, ctermfg=14 })
 hi("CorsorLineNr", { ctermbg=23, ctermfg=7 })
-hi("CursorLine", { ctermbg=236 })
+hi("CursorLine", { ctermbg=235 })
 hi("IncSearch", { ctermbg=3, ctermfg=1 })
 hi("Search", { ctermbg=1, ctermfg=3 })
 hi("Visual", { ctermbg=4, ctermfg=16 })
-hi("Pmenu", { ctermbg=240, ctermfg=12 })
-hi("PmenuSel", { ctermbg=3, ctermfg=1 })
 hi("SpellBad", { ctermbg=0, ctermfg=1 })
 hi("SpellCap", { ctermbg=0, ctermfg=15 })
 hi("VertSplit", { ctermbg=6, ctermfg=0 })
@@ -34,4 +32,24 @@ hi("TabLineSel", { ctermbg=240, ctermfg=7, term=NONE, cterm=bold })
 
 hi("GitSignsAdd", { ctermfg=76 })
 hi("GitSignsChange", { ctermfg=214 })
-hi("GitSignsCurrentLineBlame", { ctermfg=233 })
+hi("GitSignsCurrentLineBlame", { ctermfg=237 })
+
+
+-- nvim-cmp menu
+
+hi("Pmenu", { ctermbg=236 })
+-- hi("PmenuSel", { ctermbg=67 })
+hi("PmenuSel", { ctermbg=234 })
+
+-- purple
+hi("CmpItemKindFunction", { ctermfg=104 })
+hi("CmpItemKindMethod", { ctermfg=104 })
+
+-- lightblue
+hi("CmpItemKindVariable", { ctermfg=110 })
+hi("CmpItemKindInterface", { ctermfg=110 })
+hi("CmpItemKindText", { ctermfg=110 })
+
+-- blue
+hi("CmpItemAbbrMatch", { ctermfg=111 })
+hi("CmpItemAbbrMatchFuzzy", { ctermfg=111 })
