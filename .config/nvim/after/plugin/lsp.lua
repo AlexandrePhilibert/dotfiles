2071f0bda3fbf6f2de9f417281a289407d714338
Alexandre Philibert <alexandre.philibert@ik.me>
2023-02-23 09:39:15 +0100

    Add copilot

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,83 +1,82 @@
-local cmp = require('cmp')
-local lspkind = require('lspkind')
-local luasnip = require('luasnip')
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+local luasnip = require("luasnip")
 local lspconfig = require("lspconfig")
-local cmp_nvim_slp = require('cmp_nvim_lsp')
+local cmp_nvim_slp = require("cmp_nvim_lsp")
 
 cmp.setup({
-    completion = { completeopt = "menu,menuone,noinsert", keyword_length = 1 },
-    snippet = {
-        expand = function(args)
-           luasnip.lsp_expand(args.body)
-        end,
-    },
-    experimental = {
-        ghost_text = {
-            hl_group = 'Hint',
-        },
-    },
-    formatting = {
-        format = lspkind.cmp_format({
-            maxwidth = 50,
-        })
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    -- max_item_count: The maximum number of suggestions for the given source
-    -- keyword_length: The number of character to type before suggestions start
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'buffer', keyword_length = 5, max_item_count = 5 },
-    })
+	completion = { completeopt = "menu,menuone,noinsert", keyword_length = 1 },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	experimental = {
+		ghost_text = {
+			hl_group = "Hint",
+		},
+	},
+	formatting = {
+		format = lspkind.cmp_format({
+			maxwidth = 50,
+		}),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	-- max_item_count: The maximum number of suggestions for the given source
+	-- keyword_length: The number of character to type before suggestions start
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer", keyword_length = 5, max_item_count = 5 },
+	}),
 })
 
 local capabilities = cmp_nvim_slp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local function on_attach(client, bufnr)
-    local bufopts = { noremap = true, silent=true, buffer=bufnr }
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-    vim.keymap.set("n", "gh", vim.lsp.buf.hover, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, bufopts)
-    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, bufopts)
-    vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", bufopts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, bufopts)
-    vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, bufopts)
+	vim.keymap.set("n", "gh", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, bufopts)
+	vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, bufopts)
+	vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", bufopts)
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, bufopts)
 end
 
-local servers = { 'tsserver', 'tailwindcss', 'svelte', 'gopls', 'clangd' }
+local servers = { "tsserver", "tailwindcss", "svelte", "gopls", "clangd" }
 
 for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        capabilities = capabilities,
-        on_attach = on_attach
-    }
+	lspconfig[lsp].setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+	})
 end
 
-lspconfig.lua_ls.setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-}
+lspconfig.lua_ls.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
 
-lspconfig.rust_analyzer.setup {
-    cmd = { "rust-analyzer" },
-    capabilities = capabilities,
-    on_attach = on_attach,
-}
-
+lspconfig.rust_analyzer.setup({
+	cmd = { "rust-analyzer" },
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
--- .config/nvim/lua/user/packer.lua
+++ .config/nvim/lua/user/packer.lua
@@ -1,69 +1,74 @@
-require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 
-    -- Telescope
-    use 'nvim-lua/popup.nvim'
-    use 'nvim-lua/plenary.nvim'
-    use 'nvim-telescope/telescope.nvim'
+	-- Telescope
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("nvim-telescope/telescope.nvim")
 
-    -- Harpoon
-    use 'ThePrimeagen/harpoon'
+	-- Harpoon
+	use("ThePrimeagen/harpoon")
 
-    -- Lualine
-    use 'nvim-lualine/lualine.nvim'
+	-- Lualine
+	use("nvim-lualine/lualine.nvim")
 
-    -- Treesitter
-    use('nvim-treesitter/nvim-treesitter', {
-        run = ':TSUpdate'
-    })
+	-- Treesitter
+	use("nvim-treesitter/nvim-treesitter", {
+		run = ":TSUpdate",
+	})
 
-    -- Theme
-    -- use 'martinsione/darkplus.nvim'
-    use 'Mofiqul/vscode.nvim'
+	-- Theme
+	-- use 'martinsione/darkplus.nvim'
+	use("Mofiqul/vscode.nvim")
 
-    use 'lukas-reineke/indent-blankline.nvim'
+	use("lukas-reineke/indent-blankline.nvim")
 
-    -- Mason
-    use 'williamboman/mason.nvim'
+	-- Mason
+	use("williamboman/mason.nvim")
 
-    -- LSP
-    use 'hrsh7th/nvim-cmp'
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'saadparwaiz1/cmp_luasnip'
-    use 'L3MON4D3/LuaSnip'
+	-- LSP
+	use("hrsh7th/nvim-cmp")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("saadparwaiz1/cmp_luasnip")
+	use("L3MON4D3/LuaSnip")
 
-    -- LSP Icons
-    use 'onsails/lspkind.nvim'
+	-- LSP Icons
+	use("onsails/lspkind.nvim")
 
-    use 'mfussenegger/nvim-dap'
-    use 'theHamsta/nvim-dap-virtual-text'
-    use { 'rcarriga/nvim-dap-ui',
-        requires = {
-            'mfussenegger/nvim-dap'
-        }
-    }
+	use("mfussenegger/nvim-dap")
+	use("theHamsta/nvim-dap-virtual-text")
+	use({
+		"rcarriga/nvim-dap-ui",
+		requires = {
+			"mfussenegger/nvim-dap",
+		},
+	})
 
-    -- Pairs
-    use 'windwp/nvim-ts-autotag'
-    use 'windwp/nvim-autopairs'
+	-- Pairs
+	use("windwp/nvim-ts-autotag")
+	use("windwp/nvim-autopairs")
 
-    -- Prettier
-    use('jose-elias-alvarez/null-ls.nvim')
-    use('MunifTanjim/prettier.nvim')
+	-- Prettier
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("MunifTanjim/prettier.nvim")
 
-    -- Git
-    use 'lewis6991/gitsigns.nvim'
-    use 'tpope/vim-fugitive'
+	-- Git
+	use("lewis6991/gitsigns.nvim")
+	use("tpope/vim-fugitive")
 
-    -- Svelte
-    use 'evanleck/vim-svelte'
+	-- Svelte
+	use("evanleck/vim-svelte")
 
-    -- Terminal
-    use { "akinsho/toggleterm.nvim",
-        tag = '*',
-        config = function()
-            require("toggleterm").setup()
-        end
-    }
+	-- Terminal
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+		config = function()
+			require("toggleterm").setup()
+		end,
+	})
+
+	-- Copilot
+	use("github/copilot.vim")
 end)
